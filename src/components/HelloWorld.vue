0e1f3ee389ad9d638caa725610f9f74587375839
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-03-09 00:29:11 +0900

    add animation to top page title

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="hello">
+  <!-- <div class="hello">
     <h1>{{ msg }}</h1>
-  </div>
+  </div> -->
+  <h1 class="hello loader">
+    <span v-for="(t, index) in title" :key="index" v-text="t"/>
+  </h1>
 </template>
 
 <script>
@@ -9,7 +12,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: "Sai's Portfolio Site"
+      title: "Sai's Portfolio Site"
     }
   }
 }
@@ -17,13 +20,13 @@ export default {
 
 <style scoped>
 .hello {
-  background-color: #313131; /*背景色の指定*/
+  background-color: #313131;
   overflow: hidden;
-  padding-top: 200px; /*文字上部に余白*/
-  padding-bottom: 400px; /*文字下部に余白*/
+  padding-top: 200px;
+  padding-bottom: 400px;
 }
 
-h1, h2 {
+/* h1, h2 {
   color: black;
   font-weight: normal;
   font-size: 100px;
@@ -31,16 +34,119 @@ h1, h2 {
   -2px 2px 1px white,
   2px -2px 1px white,
   -2px -2px 1px white;
+} */
+
+h1.loader {
+  text-align: center;
+  font-family: 'Nunito', sans-serif;
+  font-size: 4.6875em;
+  color: transparent;
+  letter-spacing: 0.01em;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.loader span {
+  text-shadow:
+    0 0 2px rgba(204, 208, 212,0.9),
+    0 15px 25px rgba(0, 0, 0, 0.3),
+    0 -2px 3px rgba(0, 0, 0, 0.1),
+    0 -5px 10px rgba(255, 255, 255, 0.5),
+    0 5px 10px rgba(0, 0, 0, 0.3),
+    0 3px 4px rgba(255, 255, 255, 0.2),
+    0 0 20px rgba(255, 255, 255, 0.45);
+
+    animation: loading 0.85s ease-in-out infinite alternate;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+@keyframes loading {
+ to {
+   text-shadow:
+    0 0 2px rgba(204, 208, 212,0.2),
+    0 0 3px rgba(0, 0, 0, 0.02),
+    0 0 0 rgba(0, 0, 0, 0),
+    0 0 0 rgba(255, 255, 255, 0),
+    0 0 0 rgba(0, 0, 0, 0),
+    0 0 0 rgba(255, 255, 255, 0),
+    0 0 0 rgba(255, 255, 255, 0);
+  }
 }
-a {
-  color: #42b983;
+
+.loader span:nth-child(2) {
+  animation-delay:0.15s;
+}
+
+.loader span:nth-child(3) {
+  animation-delay:0.30s;
+}
+
+.loader span:nth-child(4) {
+  animation-delay:0.45s;
+}
+
+.loader span:nth-child(5) {
+  animation-delay:0.60s;
+}
+
+.loader span:nth-child(6) {
+  animation-delay:0.75s;
+}
+
+.loader span:nth-child(7) {
+  animation-delay:0.90s;
+}
+
+.loader span:nth-child(8) {
+  animation-delay:1.05s;
+}
+
+.loader span:nth-child(9) {
+  animation-delay:1.20s;
+}
+
+.loader span:nth-child(10) {
+  animation-delay:1.35s;
+}
+
+.loader span:nth-child(11) {
+  animation-delay:1.50s;
+}
+
+.loader span:nth-child(12) {
+  animation-delay:1.65s;
+}
+
+.loader span:nth-child(13) {
+  animation-delay:1.80s;
+}
+
+.loader span:nth-child(14) {
+  animation-delay:1.95s;
+}
+
+.loader span:nth-child(15) {
+  animation-delay:2.10s;
+}
+
+.loader span:nth-child(16) {
+  animation-delay:2.25s;
+}
+
+.loader span:nth-child(17) {
+  animation-delay:2.40s;
+}
+
+.loader span:nth-child(18) {
+  animation-delay:2.55s;
+}
+
+.loader span:nth-child(19) {
+  animation-delay:2.70s;
+}
+
+.loader span:nth-child(20) {
+  animation-delay:2.85s;
+}
+
+.loader span:nth-child(21) {
+  animation-delay:3.00s;
 }
 </style>
