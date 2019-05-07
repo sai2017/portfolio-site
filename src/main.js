bb31bb76f923c2cd92b61e5701c29e4cad695ee2
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-05-07 23:44:05 +0900

    fix bootstrap.min.css

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
